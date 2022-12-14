fa32b9821f00b3443bc13206406b045dcabf5d9a
Dmitriy Gorbunov <dmitriy.goto@gmail.com>
2022-12-15 00:47:30 +0300

    Implement dialog navigator.

---
+++ premo-navigation/src/commonMain/kotlin/me/dmdev/premo/navigation/DialogNavigation.kt
@@ -0,0 +1,43 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2020-2022 Dmitriy Gorbunov (dmitriy.goto@gmail.com)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package me.dmdev.premo.navigation
+
+import kotlinx.coroutines.flow.StateFlow
+import me.dmdev.premo.PmMessage
+import me.dmdev.premo.PresentationModel
+
+interface DialogNavigation<D: PresentationModel> {
+    val dialog: StateFlow<D?>
+    fun onDismissRequest()
+}
+
+val <D: PresentationModel> DialogNavigation<D>.isShowing: Boolean get() = dialog.value != null
+
+inline fun <D : PresentationModel, reified R : PmMessage> PresentationModel.DialogNavigation(
+    key: String = "dialog",
+    noinline onDismissRequest: (navigator: DialogNavigator<D, R>) -> Unit = { navigator -> navigator.dismiss() }
+): DialogNavigation<D> {
+    return DialogNavigator(key, onDismissRequest)
+}
---
+++ premo-navigation/src/commonMain/kotlin/me/dmdev/premo/navigation/DialogNavigator.kt
@@ -0,0 +1,103 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2020-2022 Dmitriy Gorbunov (dmitriy.goto@gmail.com)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package me.dmdev.premo.navigation
+
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import me.dmdev.premo.*
+
+interface DialogNavigator<D: PresentationModel, R> : DialogNavigation<D> {
+    fun show(pm: D)
+    suspend fun showForResult(pm: D): R?
+    fun sendResult(result: R)
+    fun dismiss()
+}
+
+inline fun <D : PresentationModel, reified R : PmMessage> PresentationModel.DialogNavigator(
+    key: String = "dialog",
+    noinline onDismissRequest: (navigator: DialogNavigator<D, R>) -> Unit = { navigator -> navigator.dismiss() },
+): DialogNavigator<D, R> {
+
+    val navigator = DialogNavigatorImpl(onDismissRequest)
+    messageHandler.onMessage<R> { navigator.sendResult(it) }
+    messageHandler.handle<BackMessage> {
+        if (navigator.isShowing) {
+            navigator.dismiss()
+            true
+        } else {
+            false
+        }
+    }
+
+    val savedDialogPm = stateHandler.getSaved<Pair<PmDescription, String>?>(key)
+    if (savedDialogPm != null) {
+        navigator.show(Child(savedDialogPm.first, savedDialogPm.second))
+    }
+    stateHandler.setSaver(key) {
+        navigator.dialog.value?.let { detailPm ->
+            Pair(detailPm.description, detailPm.tag)
+        }
+    }
+
+    return navigator
+}
+
+class DialogNavigatorImpl<D: PresentationModel, R : PmMessage>(
+    private val onDismissRequest: (navigator: DialogNavigator<D, R>) -> Unit
+) : DialogNavigator<D, R> {
+
+    private val _dialog: MutableStateFlow<D?> = MutableStateFlow(null)
+    override val dialog: StateFlow<D?> = _dialog.asStateFlow()
+
+    private val resultChannel = Channel<R?>()
+
+    override fun show(pm: D) {
+        if (isShowing) dismiss()
+        _dialog.value = pm
+        pm.attachToParent()
+    }
+
+    override suspend fun showForResult(pm: D): R? {
+        show(pm)
+        return resultChannel.receive()
+    }
+
+    override fun sendResult(result: R) {
+        _dialog.value = null
+        resultChannel.trySend(result)
+    }
+
+    override fun dismiss() {
+        _dialog.value?.detachFromParent()
+        _dialog.value = null
+        resultChannel.trySend(null)
+    }
+
+    override fun onDismissRequest() {
+        onDismissRequest(this)
+    }
+}
