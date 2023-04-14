ef0490223b561c4385813873f8e46550ec0fd155
Dmitriy Gorbunov <dmitriy.goto@gmail.com>
2023-04-14 23:17:44 +0300

    Detach child pm from parent when sending a dialog result.

--- premo-navigation/src/commonMain/kotlin/me/dmdev/premo/navigation/DialogNavigator.kt
+++ premo-navigation/src/commonMain/kotlin/me/dmdev/premo/navigation/DialogNavigator.kt
@@ -1,7 +1,7 @@
 /*
  * The MIT License (MIT)
  *
- * Copyright (c) 2020-2022 Dmitriy Gorbunov (dmitriy.goto@gmail.com)
+ * Copyright (c) 2020-2023 Dmitriy Gorbunov (dmitriy.goto@gmail.com)
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -78,6 +78,7 @@ class DialogNavigatorImpl<D: PresentationModel, R : PmMessage>(
     }
 
     override fun sendResult(result: R) {
+        _dialog.value?.detachFromParent()
         _dialog.value = null
         resultChannel.trySend(result)
     }
